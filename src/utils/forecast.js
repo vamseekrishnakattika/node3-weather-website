09ea8013e10d0e45b6c2ac134d033157a2c21d52
Vamseekrishna Kattika <vamseekrishna.kattika@gmail.com>
2019-03-21 15:45:34 -0500

    Init commit

---
+++ src/utils/forecast.js
@@ -0,0 +1,21 @@
+const request = require('request')
+const forecastAPIkey = '95a0deee6cf97070fce0d51399a2609a'
+
+const forecast = (latitude,longitude,callback) => {
+  const url = 'https://api.darksky.net/forecast/'+ forecastAPIkey +'/'+ latitude +','+ longitude
+  request({
+    url,
+    json:true
+  },(error,response, body) =>{
+    if (!error && response.statusCode === 200){
+      callback(undefined,
+        body.daily.data[0].summary + ' It is currently ' + body.currently.temperature + ' degrees out. There is a ' + body.currently.precipProbability + '% chance of rain.'
+      )
+    }
+    else {
+        callback('Unable to fetch weather', undefined);
+    }
+  });
+};
+
+module.exports = forecast
---
+++ src/utils/geocode.js
@@ -0,0 +1,27 @@
+const request = require('request')
+
+const geocode = (address,callback) =>{
+ const encodedAddress = encodeURIComponent(address);
+  const access_key = 'pk.eyJ1IjoidmFtc2Vla3Jpc2huYWthdHRpa2EiLCJhIjoiY2p0Z2lwdzI2MDN1dTN5cXIxZDd1czkwayJ9.1fAxtRbecQ4Lh-iTF13SIQ';
+  const url = 'https://api.mapbox.com/geocoding/v5/mapbox.places/' + encodedAddress + '.json?access_token=' + access_key
+  request({
+    url,
+    json:true
+  },(error,{ body }) =>{
+    if(error){
+      callback('Unable to connect to location servers!', undefined);
+    }
+    else if(body.features.length === 0){
+      callback('Unable to find \'' + address + '\' Try another search', undefined);
+    }
+    else {
+      callback(undefined,{
+        longitude: body.features[0].center[0],
+        latitude: body.features[0].center[1],        
+        location : body.features[0].place_name
+      });
+    }
+  });
+};
+
+module.exports = geocode
